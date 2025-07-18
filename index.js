7fba382a220d1c3e00793bc6e0a6adbddd9a3bd7
Meeth <meethukothari@gmail.com>
2025-07-18 17:06:06 +0530

    integrated swagger for documenting api

--- index.js
+++ index.js
@@ -2,17 +2,43 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const tasksRouter = require("./routes/tasks.routes");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 // initialization
 const app = express();
 dotenv.config();
 
+// swagger 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "task_app",
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000'
+            }
+        ],
+    },
+    apis: ["./routes/tasks.routes.js"]
+} 
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
 // entry point middleware
 app.use(express.json());
 app.use(cors());
 
+
 // env var
 const port = process.env.PORT;
+
 
 // routing
 app.use("/api/tasks", tasksRouter)
--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -2,10 +2,158 @@ const express = require("express");
 const router = express.Router();
 const {getAllTasks, getTaskById, createTask, updateTask, deleteTaskById} = require("../controllers/tasks.controllers");
 
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Task:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         title:
+ *           type: string
+ *         description:
+ *           type: string
+ *         completed:
+ *           type: boolean
+ */
+
+
+
+/**
+ * @swagger
+ * /api/tasks:
+ *   get:
+ *     summary: Get list of tasks
+ *     description: Returns all tasks
+ *     responses:
+ *       200:
+ *         description: List of tasks from MySQL
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ */
 router.get("/", getAllTasks);
+
+
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   get:
+ *     summary: Get a single task by ID
+ *     description: Returns a single task
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single task
+ *         content:
+ *           application/json:
+ *             schema:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Task'
+ */
 router.get("/:id", getTaskById);
+        
+
+/**
+ * @swagger
+ * /api/tasks:
+ *   post:
+ *     summary: Create a new task
+ *     description: Adds a new task to the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Task created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ */
 router.post("/", createTask);
+
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   put:
+ *     summary: Update an existing task
+ *     description: Update fields of a task by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               completed:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Task updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       404:
+ *         description: Task not found
+ */
 router.put("/:id", updateTask)
+
+
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   delete:
+ *     summary: Delete a task
+ *     description: Deletes a task by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Task deleted successfully
+ *       404:
+ *         description: Task not found
+ */
 router.delete("/:id", deleteTaskById)
 
 module.exports = router;
